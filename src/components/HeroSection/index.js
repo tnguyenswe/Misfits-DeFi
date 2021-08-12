d6fbd599961e33861635c677e80450a4833c4e18
Thomas Nguyen <tnguyenswe@gmail.com>
2021-08-11 22:58:05 -0700

    Update README, White Paper link

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -45,7 +45,7 @@ const HeroSection = () => {
                 <HeroH1>Misfits DeFi</HeroH1>
                 <HeroP>Preventing food waste and delivering low cost groceries.</HeroP>
                 <HeroBtnWrapper>
-                    <Link1 href="https://drive.google.com/file/d/19z9foncYtzas3w8BPg81GEcDGHEtIMz2/view" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+                    <Link1 href="https://drive.google.com/file/d/1SwC0mxgutbIHJ4LT-rt4IcwiU4j_CL5G/view?usp=sharing" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
                         Learn More {hover ? <ArrowForward /> : <ArrowRight/>}
                     </Link1>
                 </HeroBtnWrapper>
